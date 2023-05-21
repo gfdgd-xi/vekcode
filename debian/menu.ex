3753e04104c87043612426bdb2144dfaa08e3564
gfdgd_xi <3025613752@qq.com>
2023-05-21 08:30:46 +0800

    移除debian目录

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(org.sea-group.raa-1.0.25):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="org.sea-group.raa-1.0.25" command="/usr/bin/org.sea-group.raa-1.0.25"
--- debian/org.sea-group.raa-1.0.25.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the org.sea-group.raa-1.0.25 package
-#
-0 4	* * *	root	[ -x /usr/bin/org.sea-group.raa-1.0.25_maintenance ] && /usr/bin/org.sea-group.raa-1.0.25_maintenance
